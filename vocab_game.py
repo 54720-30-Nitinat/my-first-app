c1345f3812203ee58e815b595856f8550b5b9156
54720-30-Nitinat <54720@yupparaj.ac.th>
2026-08-24 09:12:55 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -63,7 +63,7 @@ def show_result_dialog(ans1, ans2):
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
+    if score == 4:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
