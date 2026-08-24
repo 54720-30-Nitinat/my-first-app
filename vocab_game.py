dad27a0d00f87be7a076e47f5984a063d728d183
54720-30-Nitinat <54720@yupparaj.ac.th>
2026-08-24 09:08:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -38,17 +38,17 @@ def show_result_dialog(ans1, ans2):
         
     # ตรวจข้อ 3
     if u_ans2 == "pencil":
-        st.success("✅ ข้อ 2: ถูกต้อง")
+        st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
     if u_ans2 == "pen":
-        st.success("✅ ข้อ 2: ถูกต้อง")
+        st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
         
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
