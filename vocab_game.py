b55bafb3d536b184c8104b2da8f8eebeb86ea032
54720-30-Nitinat <54720@yupparaj.ac.th>
2026-08-24 09:10:28 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -37,14 +37,14 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
         
     # ตรวจข้อ 3
-    if u_ans2 == "pencil":
+    if u_ans3 == "pencil":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans2 == "pen":
+    if u_ans4 == "pen":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
@@ -89,17 +89,17 @@ ans2 = st.text_input(
 )
 ans3 = st.text_input(
     "ข้อ 3: What do elementary students use to write `p _ n c _ l`. ✏️",
-    value=st.session_state.ans2_val,
+    value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
     "ข้อ 4: What do highschool students use to write `p _ n`. 🖊️",
-    value=st.session_state.ans2_val,
+    value=st.session_state.ans4_val,
 )
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-st.session_state.ans1_val = ans3
-st.session_state.ans2_val = ans4
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
