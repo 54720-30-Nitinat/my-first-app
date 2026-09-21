11a65d692a56d95644112dab0b91d9c6dda9d813
54720-30-Nitinat <54720@yupparaj.ac.th>
2026-09-21 13:26:41 +0700

    Create bmiApp.py

---
+++ bmiApp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.markdown("#red[ ค่านวณคาดัชนีมวลกาย BMI]")
+st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
+
+
+weight st.number_input("กรอกน่าหนักของคุณ (กิโลกรัม):", min_value-1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร) :", min_value=1.0)
+
+
+if st.button("ค่านวณค่า BMI 📝"):
+
+height_m = height_cm / 100
+bmi weight / (height_m** 2)
+
+st.write("---")
+st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+
+if bmi 18.5:
+    warning("คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+
+elif 18.5 <= bmi < 23.0:
+    st.success(" คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+
+elif 23.0 <= bmi < 25.0:
+    st.info(" คุณเริ่มมีน้ำหนักเกินเกลเฑ์ (ท้วม)")
+else:
+    st.error(" คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
+
+st.divider()
+st.write("นางสาวนิธินาถ พัฒนบุญแสน เลขที่ 30 ม.4/3")
