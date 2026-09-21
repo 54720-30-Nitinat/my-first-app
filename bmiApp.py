e47ea3b21a9aaae4bd8ef817632c30d8a06baa64
54720-30-Nitinat <54720@yupparaj.ac.th>
2026-09-21 13:33:12 +0700

    bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -10,11 +10,11 @@ height_cm = st.number_input("กรอกส่วนสูงของคุณ
 
 if st.button("ค่านวณค่า BMI 📝"):
 
-height_m = height_cm / 100
-bmi weight / (height_m** 2)
+    height_m = height_cm / 100
+    bmi weight / (height_m** 2)
 
-st.write("---")
-st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+    st.write("---")
+    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
 if bmi 18.5:
     warning("คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)📏")
