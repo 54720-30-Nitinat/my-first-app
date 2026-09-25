6a739b64d01a525b17631e59919b665c67d9ee96
54720-30-Nitinat <54720@yupparaj.ac.th>
2026-09-25 22:16:06 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,1 +1,21 @@
+import streamlit as st  #[cite: 1]
 
+st.title(
+    " แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%"
+)  #[cite: 1]
+
+price = st.number_input(
+    "กรอกราคาสินค้า (บาท):", value=0.0
+)  #[cite: 1]
+
+vat = price * 0.07  #[cite: 1]
+net_price = price - vat  #[cite: 1]
+
+st.header(
+    f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท"
+)  #[cite: 1]
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")  #[cite: 1]
+
+st.divider()  #[cite: 1]
+
+st.write("นางสาวดีใจ ยิ้มแฉล้ม เลขที่ 5 ม.4/5")  #[cite: 1]
